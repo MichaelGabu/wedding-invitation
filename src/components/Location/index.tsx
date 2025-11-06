29b74a894e4d566caf654e42ba33731654b36a34
Michael Gabu <maikygabu@gmail.com>
2025-11-06 16:28:21 -0500

    Enhance Location and Timer components with new styles and animations
    Added chapel illustration and updated Location component layout with background image. Refactored Timer component to include countdown items and separators, and integrated scroll-based animations using animejs. Updated and expanded SASS styles for Button, Location, and Timer components for improved UI consistency. Added new CSS variable for large font size.

--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -1,14 +1,22 @@
 import './location.sass';
 import Button from '../Button';
+import chapel from '../../assets/images/chapel-draw.webp';
 
 const Location = () => {
     return (
         <section className="section location">
-            <div className="section__container">
-                <h3>Ceremonia y Recepción</h3>
-                <h4>HACIENDA SAN SEBASTIAN</h4>
-                <p>Vía Subachoque - El Rosal</p>
-                <Button label="Ver ubicación" onClick={() => {}} />
+            <figure className="location__bg">
+                <img className="location__bg-image" src={chapel} alt="Capilla" />
+            </figure>
+            <div className="section__container location__container">
+                <div className="location__content">
+                    <h3 className="location__title">Ceremonia y Recepción</h3>
+                    <h4 className="location__subtitle">HACIENDA SAN SEBASTIAN</h4>
+                    <p className="location__address">Vía Subachoque - El Rosal</p>
+                </div>
+                <div className="loaction__actions">
+                    <Button label="Ver ubicación" onClick={() => {}} />
+                </div>
             </div>
         </section>
     );
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { createScope, utils, animate, onScroll, Scope, stagger } from 'animejs';
 import './timer.sass';
 
 interface TimerProps {
@@ -26,11 +27,100 @@ const Timer: React.FC<TimerProps> = ({ date }) => {
         return () => clearInterval(interval);
     }, [date]);
 
+    const root = useRef<HTMLDivElement>(null);
+    const scope = useRef<Scope | null>(null); 
+
+    useEffect(() => {
+        scope.current = createScope({ root })
+        scope.current.add(() => {
+            const $title = utils.$('.timer__title');
+            const $countdownItems = utils.$('.timer__countdown-item');
+            const $countdownSeparator = utils.$('.timer__countdown-separator');
+
+            utils.set($title, {
+                y: 50,
+                opacity: 0,
+                filter: 'blur(2px)'
+            });
+            animate($title, {
+                y: [50, 0],
+                opacity: [0, 1],
+                filter: ['blur(2px)', 'blur(0px)'],
+                duration: 1000,
+                delay: 0,
+                easing: 'easeOutQuad',
+                autoplay: onScroll({
+                    enter: 'bottom-=10% top',
+                    leave: 'top+=10% bottom',
+                    debug: false,
+                })
+            });
+
+            utils.set($countdownItems, {
+                y: 100,
+                opacity: 0,
+                filter: 'blur(2px)'
+            });
+            animate($countdownItems, {
+                y: [100, 0],
+                opacity: [0, 1],
+                filter: ['blur(2px)', 'blur(0px)'],
+                duration: 1000,
+                delay: stagger(100, { start: 0 } ),
+                easing: 'easeOutQuad',
+                autoplay: onScroll({
+                    enter: 'bottom-=5% top',
+                    leave: 'top+=5% bottom',
+                    debug: false,
+                })
+            });
+
+            utils.set($countdownSeparator, {
+                opacity: 0,
+                filter: 'blur(2px)'
+            });
+            animate($countdownSeparator, {
+                opacity: [0, 1],
+                filter: ['blur(2px)', 'blur(0px)'],
+                duration: 500,
+                delay: 1000,
+                easing: 'easeOutQuad',
+                autoplay: onScroll({
+                    enter: 'bottom-=10% top',
+                    leave: 'top+=10% bottom',
+                    debug: false,
+                })
+            });
+        });
+
+        return () => scope.current?.revert();
+    }, []);
+
     return (
-        <section className="section timer">
-            <div className="section__container">
-                <h3>Falta poco</h3>
-                <p>{timeLeft.days} días, {timeLeft.hours} horas, {timeLeft.minutes} minutos, {timeLeft.seconds} segundos</p>
+        <section className="section timer" ref={root}>
+            <div className="section__container timer__container">
+                <h3 className="timer__title">Falta poco</h3>
+                <div className="timer__countdown">
+                    <div className="timer__countdown-item">
+                        <strong>{timeLeft.days}</strong>
+                        <span>Días</span>
+                    </div>
+                    <span className="timer__countdown-separator">:</span>
+                    <div className="timer__countdown-item">
+                        <strong>{timeLeft.hours}</strong>
+                        <span>Horas</span>
+                    </div>
+                    <span className="timer__countdown-separator">:</span>
+                    <div className="timer__countdown-item">
+                        <strong>{timeLeft.minutes}</strong>
+                        <span>Minutos</span>
+                    </div>
+                    <span className="timer__countdown-separator">:</span>
+                    <div className="timer__countdown-item">
+                        <strong>{timeLeft.seconds}</strong>
+                        <span>Segundos</span>
+                    </div>
+                </div>
             </div>
         </section>
     );
